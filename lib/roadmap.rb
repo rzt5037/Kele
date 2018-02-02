41a21e3e8908d01b661ef9764d1ca7fd7459855b
Richard Travis <r.z.travis@gmail.com>
2018-02-02 08:25:27 -0700

    This is a test for checkpoint submition

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -1,16 +1,17 @@
 require 'httparty'
 require 'json'
+require "awesome_print"
 
 module Roadmap
   include HTTParty
 
   def get_roadmap(roadmap_id)
-    response = self.class.get('/roadmaps/' + roadmap_id.to_s, headers: {"authorization" => @auth_token})
+    response = self.class.get('/roadmaps/' + roadmap_id.to_s, headers: {"content_type" => 'application/json',"authorization" => @auth_token})
     JSON.parse(response.body)
   end
 
   def get_checkpoint(checkpoint_id)
-    response = self.class.get('/checkpoints/' + checkpoint_id.to_s, headers: {"authorization" => @auth_token})
+    response = self.class.get('/checkpoints/' + checkpoint_id.to_s, headers: {"content_type" => 'application/json',"authorization" => @auth_token})
     JSON.parse(response.body)
   end
 end
